1d6a130f9605fc5abf6abac3458a144c1bec51fd
Anubhav Sachan <83161041+Anubhav1432@users.noreply.github.com>
2026-02-20 15:09:53 +0530

    Initial commit: Add Python practice files

---
+++ electricity_bill.py
@@ -0,0 +1,9 @@
+units=int(input())
+bill=0
+if units<=100:
+    bill=units*3
+elif units>=101 and units<=200:
+    bill=(100*3)+((units-100)*5)
+else:
+    bill = (100*3)+(100*5)+((units-200)*7)
+print(bill)
---
+++ zero_nine.py
@@ -0,0 +1,17 @@
+num= int(input("Enter a number: "))
+dict={
+    0:"Zero",
+    1:"One",
+    2:"Two",
+    3:"Three",
+    4:"Four",
+    5:"Five",
+    6:"Six",
+    7:"Seven",
+    8:"Eight",
+    9:"Nine"
+}
+if num in dict:
+    print(dict[num])
+else:
+    print("Not exist")
